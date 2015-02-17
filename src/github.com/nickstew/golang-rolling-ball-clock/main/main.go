b2882b74aeb99eb4f7fd98166cfd6f43e12b7e9d
Nick Stewart <phoenix.fire09@gmail.com>
2015-02-17 13:05:05 -0700

    Cleaned up println

--- src/github.com/nickstew/golang-rolling-ball-clock/main/main.go
+++ src/github.com/nickstew/golang-rolling-ball-clock/main/main.go
@@ -8,7 +8,6 @@ import (
 	. "github.com/nickstew/golang-rolling-ball-clock/clock"
 )
 
-
 func ReadClocks() ([]int, error) {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(bufio.ScanWords)
@@ -27,7 +26,9 @@ func ReadClocks() ([]int, error) {
 }
 
 func main() {
-	fmt.Printf("Please enter starting balls in queue for Ball Clock then press enter.\nValid values are (27-127).\nEnter 0 to begin calculation of previously entered Ball Clocks.")
+	fmt.Println("Please enter starting balls in queue for Ball Clock then press enter.\n",
+		"Valid values are (27-127).\n",
+		"Enter 0 to begin calculation of previously entered Ball Clocks.")
 	clocks, err := ReadClocks()
 	if err != nil {
 		fmt.Println("You did not enter a valid integer between 27 and 127.  Please rerun program.")
@@ -39,4 +40,3 @@ func main() {
 		}
 	}
 }
-
